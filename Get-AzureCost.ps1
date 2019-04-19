2634f7ebc427966b1a23e9fe3e97f660aad21320
Michael Rasmussen <mirasmus@microsoft.com>
2019-04-19 15:57:14 -0400

    Changed comments

--- Get-AzureCost.ps1
+++ Get-AzureCost.ps1
@@ -23,7 +23,9 @@ function AzureLogin
         [Parameter(Mandatory=$false)]
         [string] $LoginName,
         [Parameter(Mandatory=$false)]
-        [bool] $AzureForGov = $false
+        [bool] $AzureForGov = $false,
+        [Parameter(Mandatory=$false)]
+        [bool] $ConnectToAzureAd = $false
     )
 
     try 
@@ -51,7 +53,6 @@ function AzureLogin
                 if(!(Test-Path -Path $SecurePasswordLocation)){return Write-Host "Path doesn't exist: $($SecurePasswordLocation)"; $false}
             } 
         }
-
         try 
         {
             $password = Get-Content $SecurePasswordLocation | ConvertTo-SecureString
@@ -63,19 +64,30 @@ function AzureLogin
         {
             if($success)
             {
-                if($AzureForGov){Connect-AzAccount -Credential $cred -EnvironmentName AzureUSGovernment | Out-Null}
-                else{Connect-AzAccount -Credential $cred | Out-Null}
-                $context = Get-AzContext
-                if($context.Subscription.Name){$success = $true}
-                else{$success = $false}
-                
+                #connect AD or Az
+                if($ConnectToAzureAd)
+                {
+                    if($AzureForGov){Connect-AzureAD -Credential $cred -EnvironmentName AzureUSGovernment | Out-Null}
+                    else{Connect-AzureAD -Credential $cred | Out-Null}
+                    $context = Get-AzureADUser -Top 1
+                    if($context){$success = $true}   
+                    else{$success = $false}
+                }
+                else 
+                {
+                    if($AzureForGov){Connect-AzAccount -Credential $cred -EnvironmentName AzureUSGovernment | Out-Null}
+                    else{Connect-AzAccount -Credential $cred | Out-Null}
+                    $context = Get-AzContext
+                    if($context.Subscription.Name){$success = $true}
+                    else{$success = $false}
+                }
                 if(!($success))
                 {
                   # error logging into account or user doesn't have subscription rights, exit
                   $success = $false
                   throw "Failed to login, exiting..."
                   #exit
-                }
+                }   
             }
         }
         catch{$success = $false} 
@@ -240,43 +252,57 @@ function GetCostByDays
 
 ####Begin Code - enter your code in the if statement below####
 #Variables - Add your values for the variables here, you can't leave the values blank
-[string]    $LoginName =                   ""      #Azure username, something@something.onmicrosoft.com 
-[string]    $SecurePasswordLocation =      ""      #Path and filename for the secure password file c:\Whatever\securePassword.txt
-[bool]      $RunPasswordPrompt =           $true   #Uses Read-Host to prompt the user at the command prompt to enter password.  this will create the text file in $SecurePasswordLocation.
-[bool]      $AzureForGov =                 $false   #If working with Azure for Government this should be $true
+[string]    $LoginName =                   ""           #Azure username, something@something.onmicrosoft.com 
+[string]    $SecurePasswordLocation =      ""           #Path and filename for the secure password file c:\Whatever\securePassword.txt
+[string]    $LogFileNameAndPath =          ""           #If $enabledLogFile is true, the script will write to a log file in this path.  Include FileName, example c:\whatever\file.log
+[bool]      $RunPasswordPrompt =           $false        #Uses Read-Host to prompt the user at the command prompt to enter password.  this will create the text file in $SecurePasswordLocation.
+[bool]      $AzureForGovernment =          $false       #set to $true if running cmdlets against Microsoft azure for government
+[bool]      $EnableLogFile =               $false       #If enabled a log file will be written to $LogFileNameAndPath.
+[bool]      $ConnectToAzureAd =            $false       #This will connect using Connect-AzureAd instead of Connect-AzAccount
 [bool]      $GetCostAllResources =         $true   #Gets the cost of ALL objects in ALL resource groups
-[int]       $numOfDays =                   10      # How far to calculate cost.  Value here is number of days in the past
+[int]       $numOfDays =                   30      # How far to calculate cost.  Value here is number of days in the past
 [decimal]   $total =                       0.0     # Used to calculate total cost, should leave default
 try 
 {
     
-    $success = AzureLogin -RunPasswordPrompt $RunPasswordPrompt -SecurePasswordLocation $SecurePasswordLocation -LoginName $LoginName -AzureForGov $AzureForGov
+    $success = $success = AzureLogin -RunPasswordPrompt $RunPasswordPrompt -SecurePasswordLocation $SecurePasswordLocation -LoginName $LoginName -AzureForGov $AzureForGovernment -ConnectToAzureAd $ConnectToAzureAd
     if($success)
     {
+        if($EnableLogFile){Write-Logging -Message "Login Succeeded" -LogFileNameAndPath $LogFileNameAndPath | Out-Null}
         #Login Successful
-        #Add your Azure cmdlets here ###########################################
-        "Starting calculations..."
-        $resourceGroups = Get-AzResourceGroup
-        "Cost calcualted from $($numOfDays) day(s) ago to today  `r`n"
-        foreach($resourceGroup in $resourceGroups)
+        Write-Host "Login Succeeded"
+ 
+        if($ConnectToAzureAd)
         {
-            $total = $total + (GetCostByDays $numOfDays -UseAllResourceGroups $false -ResourceGroupName $resourceGroup.ResourceGroupName)
-            $resourceGroup.ResourceGroupName + " = `t" + "{0:C}" -f $total
-            $total = 0.0
+            #run commands from AzureAd for example Get-AzureAdUser###########################
         }
-
-        if($GetCostAllResources)
+        else 
         {
-            $AllGroupsCost = GetCostByDays -NumberOfDaysBack 23 -UseAllResourceGroups $true
-            "Cost of all resource groups: " + "{0:C}" -f $AllGroupsCost + "`r`n"
-        }
+            #run commands against Azure, such as Get-AzVm####################################
+            "Starting calculations..."
+            $resourceGroups = Get-AzResourceGroup
+            "Cost calcualted from $($numOfDays) day(s) ago to today  `r`n"
+            foreach($resourceGroup in $resourceGroups)
+            {
+                $total = $total + (GetCostByDays $numOfDays -UseAllResourceGroups $false -ResourceGroupName $resourceGroup.ResourceGroupName)
+                $resourceGroup.ResourceGroupName + " = `t" + "{0:C}" -f $total
+                $total = 0.0
+            }
 
-        #End Azure cmdlets #######################################################
+            if($GetCostAllResources)
+            {
+                $AllGroupsCost = GetCostByDays -NumberOfDaysBack 23 -UseAllResourceGroups $true
+                "Cost of all resource groups: " + "{0:C}" -f $AllGroupsCost + "`r`n"
+            }
+
+            #End Azure cmdlets #######################################################
+        }
     }
     else 
     {
         #Login Failed 
-        Write-Host "Login failed"
+        Write-Host "Login Failed or No Access"
+        if($EnableLogFile){Write-Logging -Message "Login Failed or No Access" -LogFileNameAndPath $LogFileNameAndPath | Out-Null}
     }
 }
 catch 
